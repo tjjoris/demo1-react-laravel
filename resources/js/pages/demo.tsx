323190dee3833c78932a4633ddfb0bfa1dbe5b33
tjjoris <tjjoris@gmail.com>
2025-09-16 11:12:24 -0400

    added time which preserves scroll

--- resources/js/pages/demo.tsx
+++ resources/js/pages/demo.tsx
@@ -2,17 +2,21 @@
 import { join } from "path";
 import Hello from "../components/hello";
 import Layout from "../layouts/layout";
+import { Link } from "@inertiajs/react";
+
 
 
 
 function Demo() {
   return (
-    <div className="p-6">
+    <div>
       <h1 className="title">
-        hello world
+        hello world I am cool.
       </h1>
       <Hello name="Luke" />
 
+      <Link preserveScroll href="hello" className="block title mt-[1000px]">{new Date().toLocaleTimeString()}
+      </Link>
     </div>
   );
 }
