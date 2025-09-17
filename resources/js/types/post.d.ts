0d1204f2b0a1f6641af2a4ef72a3cb79d6f18bcf
tjjoris <tjjoris@gmail.com>
2025-09-17 18:23:11 -0400

    added comment header to post.d.ts

--- resources/js/types/post.d.ts
+++ resources/js/types/post.d.ts
@@ -1,4 +1,10 @@
-
+/**
+ * this is a types file which contains the types for posts and paginated posts
+ * the .d.ts means its a typescript declaration file
+ * this file is used to define the types for posts and paginated posts
+ * this file is imported in the demo.tsx file
+ * it is not compiled to javascript
+ */
 
 export interface Post {
     id: number;
