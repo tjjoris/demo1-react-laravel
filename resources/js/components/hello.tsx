62ee16f2b6014c9cdd7e296f2f57134dbaae8d24
tjjoris <tjjoris@gmail.com>
2025-09-25 12:01:28 -0400

    changed hello component message

--- resources/js/components/hello.tsx
+++ resources/js/components/hello.tsx
@@ -3,5 +3,5 @@ import React from "react";
 type HelloProps = { name: string };
 
 export default function Hello({ name }: HelloProps) {
-  return <h1 className="text-2xl font-bold">Hello, {name} 👋</h1>;
+  return <h1 className="text-2xl font-bold">This is a work in progress demo of posting, and deleting to a database using Laravel, and React for front end. It is not moderated. 👋</h1>;
 }
