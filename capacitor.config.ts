bfcbdf31da3defaf4641a7af1f463ad1a80e51ad
tjjoris <tjjoris@gmail.com>
2025-10-03 08:19:03 -0400

    initialized with capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'demo1',
+  webDir: 'dist'
+};
+
+export default config;
