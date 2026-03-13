d9366e462d0e2447e8af382f6c69b4584022a53a
tjjoris <tjjoris@gmail.com>
2026-03-13 19:05:22 -0400

    added fk to posts using add_user_id_foreign_to_posts_table_2 migration

--- database/migrations/2026_03_13_223110_add_user_id_foreign_to_posts_table.php
+++ database/migrations/2026_03_13_223110_add_user_id_foreign_to_posts_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->text('body');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+            //
         });
     }
 
@@ -23,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
     }
 };
